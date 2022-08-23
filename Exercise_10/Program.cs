43eba32a34a40844d1d92ac40f86f773a0832058
Сергей Соколов <sergsok1984@gmail.com>
2022-08-23 17:29:06 +0300

    add Exercise 10

---
+++ Exercise_10/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите трехзначное число: ");
+int SecondDigit (int a) 
+{
+    a = Math.Abs(a);
+    return a / 10 % 10;
+}
+Console.WriteLine(SecondDigit(Convert.ToInt32(Console.ReadLine())));
