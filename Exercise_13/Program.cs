a6b653681b44a5837e592ca526500d5ba36faa61
Сергей Соколов <sergsok1984@gmail.com>
2022-08-24 07:51:08 +0300

    add Exercise 13

---
+++ Exercise_13/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число: ");
+int ThirdDigit(int a) 
+{
+int result = -1;
+a = Math.Abs(a);
+    if (a < 100) 
+    {
+        Console.Write("Третьей цифры нет: ");
+    } 
+    else 
+    {
+        while (a > 999) 
+        a /= 10;        
+        result = a % 10;
+    }
+return result;
+}
+Console.WriteLine(ThirdDigit(Convert.ToInt32(Console.ReadLine())));
