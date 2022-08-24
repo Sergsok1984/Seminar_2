c14f3788b9be15b774befbd7dd9610bd1ac33312
Сергей Соколов <sergsok1984@gmail.com>
2022-08-24 08:59:28 +0300

    add Exercise 15

---
+++ Exercise_15/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите день недели от 1 до 7: ");
+string Holiday(int dayNumber) 
+{
+    if (dayNumber == 6 || dayNumber == 7) 
+    {
+        return "Выходной день.";       
+    }
+    else if (dayNumber >= 1 && dayNumber <= 5)
+    {
+        return "Рабочий день.";        
+    }
+    else 
+    { 
+        return "Введено некорректное число.";        
+    }
+}
+Console.WriteLine(Holiday(Convert.ToInt32(Console.ReadLine())));
